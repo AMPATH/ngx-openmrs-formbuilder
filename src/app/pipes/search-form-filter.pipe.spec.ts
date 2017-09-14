6a220570690ccf7080748a450c35349f32748796
Fatma Ali <tumz92@gmail.com>
2017-09-14 15:32:59 +0300

    FOR-20:Add filter to forms list

---
+++ src/app/pipes/search-form-filter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SearchFormFilterPipe } from './search-form-filter.pipe';
+
+describe('SearchFormFilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SearchFormFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
