1bba767cf667503b6ec384c241934cfacb039b7c
Fatma Ali <tumz92@gmail.com>
2017-09-23 15:26:46 +0300

    updated bootstrap url

--- src/app/view-forms/view-forms.component.ts
+++ src/app/view-forms/view-forms.component.ts
@@ -21,8 +21,7 @@ export class ViewFormsComponent implements OnInit {
 
  
   ngOnInit(){
-    console.log(this.forms.length);
-    if(this.forms.length==0)
+
 	  this.fetchAllFormsService.fetchAllPOCForms().subscribe(forms =>{
     let f = forms.results;
     f.forEach((form,index) =>{
