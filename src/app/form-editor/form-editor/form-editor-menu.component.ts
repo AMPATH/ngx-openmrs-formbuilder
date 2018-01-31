186f8afb782cd47156ed2be846b9000426bc25b5
Fatma Ali <tumz92@gmail.com>
2018-01-31 15:16:30 +0300

    Feature to add concept mappings and concept ids to schema

---
+++ src/app/form-editor/form-editor/form-editor-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'form-editor-menu',
+    templateUrl: './form-editor-menu.component.html',
+    styleUrls: []
+})
+export class MenuComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
